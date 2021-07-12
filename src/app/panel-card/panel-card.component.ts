f228d12e27616d09edf3b898d8867d1c5047acd5
Patryk Langer <patryklanger@icloud.com>
2021-07-12 10:52:41 +0200

    card added

--- src/app/panel-card/panel-card.component.ts
+++ src/app/panel-card/panel-card.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-panel-card',
   templateUrl: './panel-card.component.html',
-  styleUrls: ['./panel-card.component.scss']
+  styleUrls: ['./panel-card.component.scss'],
 })
 export class PanelCardComponent implements OnInit {
+  @Input() name = '';
+  @Input() description = '';
+  @Input() imgSource = '';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/user-panel-main/user-panel-main.component.ts
+++ src/app/user-panel-main/user-panel-main.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-panel-main',
   templateUrl: './user-panel-main.component.html',
-  styleUrls: ['./user-panel-main.component.scss']
+  styleUrls: ['./user-panel-main.component.scss'],
 })
 export class UserPanelMainComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
