86149900fa8ce3100cc266d8227c92d02ba6c4c8
Patryk Langer <p.langer@wasko.pl>
2021-08-04 14:37:34 +0200

    timers imrpoved pipes improved task details implemented user's operations implemented suspending and finishing task implemented stoping timer implemented

---
+++ src/app/users/banned-users/banned-users.component.ts
@@ -0,0 +1,48 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import * as GlobalVariables from '../../globals';
+
+@Component({
+  selector: 'app-banned-users',
+  templateUrl: './banned-users.component.html',
+  styleUrls: ['./banned-users.component.scss'],
+})
+export class BannedUsersComponent implements OnInit {
+  dataFetched = false;
+
+  users: {
+    userId: number;
+    userName: string;
+    firstName: string;
+    lastName: string;
+    position: string;
+    email: string;
+    role: string;
+  }[] = [];
+  headers = new HttpHeaders();
+  path = GlobalVariables.GlobalServerPath;
+  subscription = new Subscription();
+  response$ = new Observable<any>();
+  constructor(private http: HttpClient) {
+    this.headers = this.headers.append('Content-Type', 'application/json');
+    this.headers = this.headers.append('Accept', 'application/json');
+
+    this.response$ = this.http.get(
+      this.path + GlobalVariables.UsersPath + '/disabled',
+      {
+        headers: this.headers,
+      },
+    );
+  }
+  onUserDelete(userId: number) {}
+  ngOnInit(): void {
+    this.subscription = this.response$.subscribe((res) => {
+      this.users = [...res];
+
+      this.dataFetched = true;
+      console.log(this.users);
+      console.log(this.dataFetched);
+    });
+  }
+}
